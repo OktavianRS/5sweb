12efdd0c67d3a29d7d00ed28f7906aee2764bbee
alex <alex>
2017-04-26 16:11:11 +0300

    workplaces

---
+++ js/controllers/workplaces.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alex on 26.04.2017.
+ */
+angular
+    .module('app')
+    .controller('workplacesCtrl', workplacesCtrl)
+
+workplacesCtrl.$inject = ['$scope'];
+function departmentsCtrl($scope) {
+
+}
