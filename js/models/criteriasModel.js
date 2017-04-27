381a3fc86eb65dbdf1d0a561bdc6dfa57bbeceb0
alex <alex>
2017-04-27 17:11:00 +0300

    'servise_criterias'

--- js/models/criteriasModel.js
+++ js/models/criteriasModel.js
@@ -1,48 +1,48 @@
-angular.module('model.workplaces', [])
-    .service('workplacesModel', ['url', 'api', 'toast', '$q', '$location', '$sessionStorage',
-      function(url, api, toast, $q, $location, $sessionStorage) {
+angular.module('model.criterias', [])
+    .service('criteriasModel', ['url', 'api', 'toast', '$q', '$location', '$sessionStorage',
+        function(url, api, toast, $q, $location, $sessionStorage) {
 
-      	this.createWorkplace = function(req, callback) {
-          api.post(
-            url.createWorkplace,
-            req,
-            function(res) {
-              if (typeof res.created_at !== 'undefined') {
-                toast('success', 'Created successfully', '');
-                callback();
-              } else {
-                toast('error', 'Some error occured', 'workplace not created');
-              }
-            })
-        }
-
-        this.fetchWorkplaces = function(callback) {
-          api.get(
-            url.fetchWorkplaces,
-            {},
-            function(res) {
-              callback(res);
-            })
-        }
-
-        this.deleteWorkplace = function(req, callback) {
-          api.delete(
-            url.deleteWorkplace,
-            req,
-            function() {
-              callback();
+            this.createCriteria = function(req, callback) {
+                api.post(
+                    url.createCriteria
+                req,
+                    function(res) {
+                        if (typeof res.created_at !== 'undefined') {
+                            toast('success', 'Created successfully', '');
+                            callback();
+                        } else {
+                            toast('error', 'Some error occured', 'criteria not created');
+                        }
+                    })
             }
-          );
-        }
 
-        this.updateWorkplace = function(req, callback) {
-          api.put(
-            url.updateWorkplace,
-            req,
-            function(res) {
-              callback();
+            this.fetchCriterias = function(callback) {
+                api.get(
+                    url.fetchCriterias,
+                    {},
+                    function(res) {
+                        callback(res);
+                    })
             }
-          );
-        }
 
-      }])
+            this.deleteCriteria = function(req, callback) {
+                api.delete(
+                    url.deleteCriteria,
+                    req,
+                    function() {
+                        callback();
+                    }
+                );
+            }
+
+            this.updateCriteria = function(req, callback) {
+                api.put(
+                    url.updateCriteria,
+                    req,
+                    function(res) {
+                        callback();
+                    }
+                );
+            }
+
+        }])
