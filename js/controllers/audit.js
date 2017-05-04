29e74a014b486a7f6889d9cc257048957f21c3e4
alex <alex>
2017-05-04 17:47:20 +0300

    'audit'

--- js/controllers/audit.js
+++ js/controllers/audit.js
@@ -2,9 +2,10 @@ angular
 .module('app')
 .controller('auditCtrl', auditCtrl)
 
-auditCtrl.$inject = ['$scope', 'toast', 'ngDialog', 'usersModel', 'auditModel'];
-function auditCtrl($scope, toast, ngDialog, usersModel, auditModel) {
+auditCtrl.$inject = ['$scope', 'toast', 'ngDialog', 'usersModel', 'auditModel', 'departmentsModel'];
+function auditCtrl($scope, toast, ngDialog, usersModel, auditModel, departmentsModel) {
   $scope.auditState = true;
+  $scope.search = [];
   $scope.scoreSlider = {
     value: 50,
     options: {
@@ -24,17 +25,36 @@ function auditCtrl($scope, toast, ngDialog, usersModel, auditModel) {
   $scope.audit = {
     name: '',
     description: '',
+    target: $scope.scoreSlider.value,
+    place_id: '',
+    user_id: '',
   }
 
   // fetch all initial data
   function constuctor() {
     usersModel.fetchUsers(function(result) {
       $scope.usersList = result;
+      $scope.search.user =  $scope.usersList[0];
+
+      console.log(result);
     });
 
     auditModel.fetchAudits(function(result) {
       $scope.auditList = result;
       console.log(result);
+    });
+
+    departmentsModel.fetchPlacesList(function(result) {
+      $scope.departmentsList = result;
+      $scope.search.department = $scope.departmentsList[0];
+
+      if ($scope.search.department.places.length === 0) {
+        $scope.search.department.places.push({name:'List is empty'});
+      }
+      $scope.workPlacesList = $scope.search.department.places;
+      $scope.search.workplace = $scope.workPlacesList[0];
+      console.log(result);
+
     });
   }
   constuctor();
@@ -43,7 +63,19 @@ function auditCtrl($scope, toast, ngDialog, usersModel, auditModel) {
     $scope.auditState = !$scope.auditState;
   }
 
+  $scope.selectDepartment = function (item) {
+    if (item.places.length === 0) {
+      item.places.push({name:'List is empty'});
+    }
+    $scope.workPlacesList = item.places;
+    $scope.search.workplace = $scope.workPlacesList[0];
+
+  }
+
   $scope.submit = function() {
+    debugger
+    $scope.audit.place_id = $scope.search.workplace.id;
+    $scope.audit.user_id = $scope.search.user.id;
     auditModel.startAudit($scope.audit, constuctor);
   }
 
@@ -52,7 +84,7 @@ function auditCtrl($scope, toast, ngDialog, usersModel, auditModel) {
   // }
 
   $scope.stopAudit = function(id, email, username, password) {
-    usersModel.stopAudit({id, email, username, password}, constuctor);
+    // usersModel.stopAudit({id, email, username, password}, constuctor);
   }
 
 
